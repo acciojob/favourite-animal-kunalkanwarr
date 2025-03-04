ee2dfd1a703a9d485eee6de88a8c55145f978be4
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-05 01:38:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+describe('Check if image exists and loads', () => {
+  it('loads the panda image', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#pandaImage').should(($img) => {
+      expect($img[0].naturalWidth).to.be.gt(0);
+    });
+  });
+});
